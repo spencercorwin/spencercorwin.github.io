7c0088d11e0e35890c18983c6f0fbbacbc01f6de
speenzor <spencercorwin@mac.com>
2018-07-28 22:17:19 -0700

    Adds whole new page for HOA Market Analysis app

--- hoa-market-analysis/scripts/index.js
+++ hoa-market-analysis/scripts/index.js
@@ -1,6 +1,8 @@
-var currentYear = new Date();
-
-document.getElementById('copyright').innerHTML = "COPYRIGHT &copy; Spencer Corwin " + currentYear.getFullYear()
-
-//var myHeading = document.querySelector('h1');
-//myHeading.textContent = 'Hello world! TESTTTTTT';
+function myFunction() {
+  const street = document.getElementById('input').elements[0].value;
+  const city = document.getElementById('input').elements[1].value;
+  const state = input.elements[2].value;
+  const zip = input.elements[3].value;
+  document.getElementById('demo').innerHTML = street+"<br>"+city;
+  //todo - Look at Python implementation to determine API to use
+}
