30602c0ed4554fb18ad38e36532e97ed2bf60f31
speenzor <spencercorwin@mac.com>
2018-07-28 17:44:38 -0700

    Adds a new page plus updates the developer page and homepage

---
+++ hoa-market-analysis/scripts/index.js
@@ -0,0 +1,6 @@
+var currentYear = new Date();
+
+document.getElementById('copyright').innerHTML = "COPYRIGHT &copy; Spencer Corwin " + currentYear.getFullYear()
+
+//var myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world! TESTTTTTT';
