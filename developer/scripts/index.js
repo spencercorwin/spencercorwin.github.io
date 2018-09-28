683493f0cc1f5f24c149d110106e057457944110
speenzor <spencercorwin@mac.com>
2018-09-27 21:14:40 -0700

    Updates home page to reflect new outlook and deletes special, redundant developer folder

--- developer/scripts/index.js
+++
@@ -1,6 +0,0 @@
-var currentYear = new Date();
-
-document.getElementById('copyright').innerHTML = "COPYRIGHT &copy; Spencer Corwin " + currentYear.getFullYear()
-
-//var myHeading = document.querySelector('h1');
-//myHeading.textContent = 'Hello world! TESTTTTTT';
