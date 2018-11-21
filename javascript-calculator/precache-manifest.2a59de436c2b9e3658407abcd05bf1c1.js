d097f4fc4deb1a7f6d8d752085986e3a72390c49
Spencer Corwin <spencercorwin@icloud.com>
2018-11-20 17:04:45 -0800

    Fixes JS calc deployment

---
+++ javascript-calculator/precache-manifest.2a59de436c2b9e3658407abcd05bf1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bc7773250f7c400c052",
+    "url": "/javascript-calculator/static/js/runtime~main.9bc77732.js"
+  },
+  {
+    "revision": "e55032d5ce0e320ad259",
+    "url": "/javascript-calculator/static/js/main.e55032d5.chunk.js"
+  },
+  {
+    "revision": "09371c39ca9121e19d54",
+    "url": "/javascript-calculator/static/js/1.09371c39.chunk.js"
+  },
+  {
+    "revision": "e55032d5ce0e320ad259",
+    "url": "/javascript-calculator/static/css/main.41d4245f.chunk.css"
+  },
+  {
+    "revision": "2a593abccaaed599e2d9c2843409d19f",
+    "url": "/javascript-calculator/index.html"
+  }
+];
--- javascript-calculator/service-worker.js
+++ javascript-calculator/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8954a153e4b4fb59a93b8fe92b95b97a.js"
+  "/javascript-calculator/precache-manifest.2a59de436c2b9e3658407abcd05bf1c1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/javascript-calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
