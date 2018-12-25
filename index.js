9540dbfd69c8a8ea3e3530f8a2cf8f76ef631f03
Spencer Corwin <spencercorwin@mac.com>
2018-12-25 02:45:40 -0800

    Adds FCC certificates

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ coll.addEventListener('click', () => {
         element.style.maxHeight = content.scrollHeight + 10 + 'px';
     }
 });
+
+window.onload = () => {
+    fetch('https://www.codewars.com/api/v1/users/scorwin', {
+        method: 'GET',
+        mode: 'no-cors',
+        cache: 'no-cache'
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            document.getElementById('codewars').innerHTML = (
+               'Honor: ' + json.honor + '<br>' + 'Overall kyu' + json.ranks.overall.name +
+               '<br>' + 'Python Rank: ' + json.ranks.languages.python.name + '<br>' + 
+               'Javascript Rank: ' + json.ranks.language.javascript.name + '<br>' + 
+               'Total Completed Challenges: ' + json.codeChallenges.totalCompleted 
+            );
+        })
+}
