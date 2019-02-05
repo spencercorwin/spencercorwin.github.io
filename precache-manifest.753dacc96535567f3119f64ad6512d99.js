8f47f836c7b4c7de2f281118718d3acd70d19d25
Spencer Corwin <spencercorwin@mac.com>
2019-02-05 11:22:09 -0800

    Updates the spencercorwin.com homepage with all new React/React Router implementation. Looks so baller. Except form may not work. Needs testing

---
+++ precache-manifest.753dacc96535567f3119f64ad6512d99.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5fca47b19c14057d29277ea7909220b",
+    "url": "/static/media/freecodecamp-apis-and-microservices-certification.d5fca47b.png"
+  },
+  {
+    "revision": "4201841aa2d1d1e23af6",
+    "url": "/static/css/main.f845fdc6.chunk.css"
+  },
+  {
+    "revision": "46bac5c537fd9ed1eeb5",
+    "url": "/static/js/1.46bac5c5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51af6710a9995178775b0221dd912ada",
+    "url": "/static/media/guatape-min.51af6710.jpeg"
+  },
+  {
+    "revision": "6a592e025fe46e01f7309cd166d24f2c",
+    "url": "/static/media/spencer-corwin-resume.6a592e02.png"
+  },
+  {
+    "revision": "ce7d44ea90f6da439e74d16b9cdaacb9",
+    "url": "/static/media/freecodecamp-full-stack-certification.ce7d44ea.png"
+  },
+  {
+    "revision": "4201841aa2d1d1e23af6",
+    "url": "/static/js/main.4201841a.chunk.js"
+  },
+  {
+    "revision": "684c67a5685cb001a2925000a2e05070",
+    "url": "/static/media/freecodecamp-data-visualization-certification.684c67a5.png"
+  },
+  {
+    "revision": "41884bdda05f692dbeb4b163b3be1a87",
+    "url": "/static/media/freecodecamp-front-end-libraries-certification.41884bdd.png"
+  },
+  {
+    "revision": "2158002b6b369654433432bc239f3253",
+    "url": "/static/media/freecodecamp-information-security-and-quality-assurance-certification.2158002b.png"
+  },
+  {
+    "revision": "ae47c4e2c32f7ffa8115404729c6a607",
+    "url": "/static/media/freecodecamp-javascript-algorithms-and-data-structures-certification.ae47c4e2.png"
+  },
+  {
+    "revision": "c63aeee7a7049c90537dc42f4eadd7e1",
+    "url": "/static/media/freecodecamp-responsive-web-design-certification.c63aeee7.png"
+  },
+  {
+    "revision": "ff1d3f5367b96cac3b64cd4d835bd617",
+    "url": "/index.html"
+  }
+];
