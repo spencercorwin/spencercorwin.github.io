d502fd7fba5533721b4d5298c7ed0681b4223403
Spencer Corwin <spencercorwin@mac.com>
2019-02-10 16:18:31 -0800

    Add developer directory identical to root

---
+++ developer/precache-manifest.2125084f7e45fd4b0508843633b358de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5fca47b19c14057d29277ea7909220b",
+    "url": "/static/media/freecodecamp-apis-and-microservices-certification.d5fca47b.png"
+  },
+  {
+    "revision": "2e03373d4df45bda0eac",
+    "url": "/static/css/main.c31371d6.chunk.css"
+  },
+  {
+    "revision": "cdf310daeb04c5742240",
+    "url": "/static/js/1.cdf310da.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51af6710a9995178775b0221dd912ada",
+    "url": "/static/media/guatape-min.51af6710.jpeg"
+  },
+  {
+    "revision": "147ddb21da7dfc990ee1cdcaf6d89f31",
+    "url": "/static/media/spencer-corwin-resume.147ddb21.png"
+  },
+  {
+    "revision": "ce7d44ea90f6da439e74d16b9cdaacb9",
+    "url": "/static/media/freecodecamp-full-stack-certification.ce7d44ea.png"
+  },
+  {
+    "revision": "2e03373d4df45bda0eac",
+    "url": "/static/js/main.2e03373d.chunk.js"
+  },
+  {
+    "revision": "684c67a5685cb001a2925000a2e05070",
+    "url": "/static/media/freecodecamp-data-visualization-certification.684c67a5.png"
+  },
+  {
+    "revision": "41884bdda05f692dbeb4b163b3be1a87",
+    "url": "/static/media/freecodecamp-front-end-libraries-certification.41884bdd.png"
+  },
+  {
+    "revision": "2158002b6b369654433432bc239f3253",
+    "url": "/static/media/freecodecamp-information-security-and-quality-assurance-certification.2158002b.png"
+  },
+  {
+    "revision": "ae47c4e2c32f7ffa8115404729c6a607",
+    "url": "/static/media/freecodecamp-javascript-algorithms-and-data-structures-certification.ae47c4e2.png"
+  },
+  {
+    "revision": "c63aeee7a7049c90537dc42f4eadd7e1",
+    "url": "/static/media/freecodecamp-responsive-web-design-certification.c63aeee7.png"
+  },
+  {
+    "revision": "52e48bb5b4a2185202e5e4e8480c0261",
+    "url": "/index.html"
+  }
+];
