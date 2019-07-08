e00e592bd18981b301a9bf71d0edc3d163d35d5d
Spencer Corwin <spencercorwin@icloud.com>
2019-07-07 23:56:54 -0700

    Update again same

---
+++ precache-manifest.b515a240370a6bd83b6b72bed889925d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3b8507872c5517f0976",
+    "url": "/static/css/main.c8528201.chunk.css"
+  },
+  {
+    "revision": "b3b8507872c5517f0976",
+    "url": "/static/js/main.b3b85078.chunk.js"
+  },
+  {
+    "revision": "e2ad1aea377fa0392960",
+    "url": "/static/js/1.e2ad1aea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51af6710a9995178775b0221dd912ada",
+    "url": "/static/media/guatape-min.51af6710.jpeg"
+  },
+  {
+    "revision": "9519187dd8181d6b74eb7dbea39cd5d6",
+    "url": "/index.html"
+  }
+];
